3be4caead4e9bc579afb1972b7c5d5912792b2d6
Feddane <chafeddane@gmail.com>
2023-04-24 11:30:08 +0100

    create Stories Activity

--- app/src/main/java/com/example/storyland/MainActivity.java
+++ app/src/main/java/com/example/storyland/MainActivity.java
@@ -1,16 +1,28 @@
 package com.example.storyland;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends BaseActivity {
-
-
+    Button enterButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        enterButton = findViewById(R.id.myButton);
+
+        enterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Stories.class);
+                startActivity(intent);
+            }
+        });
 
 
         }
---
+++ app/src/main/java/com/example/storyland/Stories.java
@@ -0,0 +1,14 @@
+package com.example.storyland;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Stories extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stories);
+    }
+}

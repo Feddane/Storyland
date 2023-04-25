81e3c28d6ff59fd5f9a412e8e6cb58d325d16c24
Feddane <chafeddane@gmail.com>
2023-04-25 15:18:23 +0100

    add stories and images

--- app/src/main/java/com/example/storyland/StoriesActivity.java
+++ app/src/main/java/com/example/storyland/StoriesActivity.java
@@ -27,9 +27,16 @@ public class StoriesActivity extends BaseActivity {
     }
 
     public void setData(){
-        storyList.add(new Story(R.drawable.easterbunny, "The Easter Bunny School", "By Feddane Chaima"));
-        storyList.add(new Story(R.drawable.lilypad, "Where's My Lily Pad?", "By Feddane Chaima"));
-        storyList.add(new Story(R.drawable.bears, "Golidocks and the Four Bears!", "By Feddane Chaima"));
+        storyList.add(new Story(R.drawable.story1, "The Easter Bunny School", "By Andrea Kaczmarek"));
+        storyList.add(new Story(R.drawable.story2, "Golidocks and the Four Bears!", "By Jade Maitre"));
+        storyList.add(new Story(R.drawable.story3, "The World’s Best Ice Cream", "By Andrea Kaczmarek"));
+        storyList.add(new Story(R.drawable.story4, "The Princess Who Wasn’t", "By Templeton Moss"));
+        storyList.add(new Story(R.drawable.story5, "My Friend, Funny", "By Jade Maitre"));
+        storyList.add(new Story(R.drawable.story6, "The Neighbourhood Sofa", "By Yesim Serce"));
+        storyList.add(new Story(R.drawable.story7, "Frog’s Starry Wish", "By Sheesh Pal"));
+        storyList.add(new Story(R.drawable.story8, "Mama What’s For Lunch?", "By Sally Partdridge"));
+        storyList.add(new Story(R.drawable.story9, "The Things That Really Matter", "By Refiloe Moahloli"));
+        storyList.add(new Story(R.drawable.story10, "I Love My Mom ", "By Stephony Wail"));
     }
 
     public void prepareRecyclerView(){

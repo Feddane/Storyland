a4d454dcf942582411dd8e2e4d148c79f29152e2
Feddane <chafeddane@gmail.com>
2023-04-26 14:39:14 +0100

    Create Scene.java

---
+++ app/src/main/java/Scene.java
@@ -0,0 +1,28 @@
+import android.widget.TextView;
+
+public class Scene {
+    private int imageScene;
+    private String text;
+
+    public Scene(int imageScene, String text) {
+        this.imageScene = imageScene;
+        this.text = text;
+    }
+
+
+    public int getImageScene() {
+        return imageScene;
+    }
+
+    public void setImageScene(int imageScene) {
+        this.imageScene = imageScene;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
--- app/src/main/java/com/example/storyland/ScenesActivity.java
+++ app/src/main/java/com/example/storyland/ScenesActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class ScenesActivity extends AppCompatActivity {
+public class ScenesActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
